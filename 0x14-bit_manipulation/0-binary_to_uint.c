52f70f5198ce166bbe2f77310e0f9148068b5238
Dominic Msukwa <domimsukwa.dm@gmail.com>
2023-10-26 16:56:33 +0000

    initialize project and add solution 0.0

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,52 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - function to convert binary to uint
+ * @b: a string of 0 and 1 chars
+ *
+ * Return: the converted number, or 0 if
+ * there is one or more chars in the string b that is not 0 or 1
+ * b is NULL
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+
+	unsigned int result;
+	int counter;
+	int size;
+	int flag;
+
+	if (b == NULL)
+		return (0);
+
+	flag = 0;
+	size = 0;
+
+	while (*b)
+	{
+		if ((*b != '0' && *b != '1') && (flag == 0))
+			flag = 1;
+
+		size += 1;
+		b++;
+	}
+
+	if (flag == 1)
+		return (0);
+
+	b -= size;
+
+	counter = 0;
+	result = 0;
+
+	for(; counter < size; counter++)
+	{
+		if (*b == '1')
+			result += 1 << (size - (counter + 1));
+
+		b++;
+	}
+	
+	return (result);
+}
---
+++ 0x14-bit_manipulation/README.md
@@ -0,0 +1,9 @@
+Project: C - Bit Manipulation
+0.0. convert given binary number to unsigned int
+1.1. print the binary representation of a number
+2.10. return the value of a bit at a given index
+3.11. set the value of a bit to 1 at a given index
+4.100. set the value of a bit to 0 at a given index
+5.101. return number of bits needed to flip to get from one number to another
+6. check the endianness
+7. the password for a given program
