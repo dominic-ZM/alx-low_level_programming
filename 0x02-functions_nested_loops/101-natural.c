0fdf6963869b98e350ee4a43a6a894c59b8636f6
dominic-ZM <domimsukwa.dm@gmail.com>
2024-02-20 12:18:18 +0000

    add solution 13

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * main - computes and prints the sum of all the multiples of 3 or 5 below 1024
+ * @void: takes no arguments
+ *
+ * Description: 1024 (excluded), followed by a new line.
+ *
+ * Return: 0 success, 1 otherwise
+ */
+
+int main(void)
+{
+	int num;
+	int factor_sum;
+
+	factor_sum = 0;
+	num = 1;
+
+	for (; num < 10; num++)
+	{
+		if (num % 3 == 0 || num % 5 == 0)
+			factor_sum = factor_sum + num;
+	}
+	printf("%d\n", factor_sum);
+	return (0);
+}
