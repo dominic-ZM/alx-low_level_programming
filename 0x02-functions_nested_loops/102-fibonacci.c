8fdfc853584298afc662e4e740e5e7492913f805
dominic-ZM <domimsukwa.dm@gmail.com>
2024-02-21 02:36:15 +0000

    add revised solution 14

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,44 +1,104 @@
-#include <stdio.h>
-#include <gmp.h>
+#include "main.h"
+
+
+#define MAX_DIGITS 1000
 
 /**
- * main - prints the first 50 Fibonacci numbers followed by a new line
+ * add - adds two numbers
+ * @result: the outcome of the operation
+ * @num1: first number to add
+ * @num2: second number to add
  *
- * Description: Start with 1 and 2
- * Must be separated by comma, followed by a space.
- *
- * Return: Always 0
+ * Return: none
  */
+
+void add(int result[], int num1[], int num2[])
+{
+	int carry;
+	int i;
+
+	carry = 0;
+	i = MAX_DIGITS - 1;
+
+	for (; i >= 0; i--)
+	{
+		result[i] = num1[i] + num2[i] + carry;
+		carry = result[i] / 10;
+		result[i] %= 10;
+	}
+}
+
+/**
+ * copy - copies content from one array to another
+ * @dest: the receiving array
+ * @src: the source of the data
+ *
+ * Return: none
+ */
+
+void copy(int dest[], int src[])
+{
+	int i;
+
+	i = 0;
+
+	for (; i < MAX_DIGITS; i++)
+		dest[i] = src[i];
+}
+
+/**
+ * print_number - prints a number's representation from an array
+ * @num: the array representing the number
+ *
+ * Return: none
+ */
+
+void print_number(int num[])
+{
+	int i;
+
+	i = 0;
+
+	while (i < MAX_DIGITS && num[i] == 0)
+		i++;
+	if (i == MAX_DIGITS)
+	{
+		printf("0");
+		return;
+	}
+	for (; i < MAX_DIGITS; i++)
+		printf("%d", num[i]);
+}
+
+/**
+ * main - entry point of program
+ * @void: main takes no argument
+ *
+ * Return: 0 if success
+ */
+
 int main(void)
 {
-	mpz_t previous;
-	mpz_t current;
-	mpz_t next;
-	char *previous_str;
-	char *current_str;
-	int counter;
+	int fib1[MAX_DIGITS] = {0};
+	int fib2[MAX_DIGITS] = {0};
+	int fib_next[MAX_DIGITS] = {0};
+	int i;
 
-	mpz_init_set_ui(previous, 1);
-	mpz_init_set_ui(current, 2);
+	fib1[MAX_DIGITS - 1] = 1;
+	fib2[MAX_DIGITS - 1] = 2;
 
-	previous_str = mpz_get_str(NULL, 10, previous);
-	current_str = mpz_get_str(NULL, 10, current);
+	printf("%d, %d", 1, 2);
 
-	printf("%s, %s", previous_str, current_str);
+	i = 3;
 
-	counter = 0;
-
-	for (; counter < 48; counter++)
+	for (; i <= 50; i++)
 	{
-		mpz_init(next);
-		mpz_add(next, previous, current);
-		gmp_printf(", %Zd", next);
-		mpz_set(previous, current);
-		mpz_set(current, next);
-		mpz_clear(next);
+		add(fib_next, fib1, fib2);
+		copy(fib1, fib2);
+		copy(fib2, fib_next);
+		printf(", ");
+		print_number(fib_next);
 	}
 	printf("\n");
-	mpz_clear(previous);
-	mpz_clear(current);
 	return (0);
 }
