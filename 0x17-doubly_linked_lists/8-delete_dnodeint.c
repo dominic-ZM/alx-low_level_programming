17118f8655715643301b5e23ece179f592e2e699
dominic-ZM <domimsukwa.dm@gmail.com>
2024-08-15 03:05:02 +0000

    add solutions for madatory tasks

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * delete_dnodeint_at_index - deletes the node at index index of a dlistint_t linked list
+ * delete_dnodeint_at_index - deletes node at index of dlistint_t linked list
  * @head: head pointer of list to modify
  * @index: position of target node to delete
  *
